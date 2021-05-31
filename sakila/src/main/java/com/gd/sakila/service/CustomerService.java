08ba38f61f60b0a22bec946d81bf754ea3379f0f
JUNG-AH-C <wjddk718@gmail.com>
2021-05-31 17:37:43 +0900

    고객 목록 출력 기능 구현 중 임시 커밋 -> 지금까지 vo, mapper, service 구현했고, controller와 view를 작성해야 함

--- sakila/src/main/java/com/gd/sakila/service/CustomerService.java
+++ sakila/src/main/java/com/gd/sakila/service/CustomerService.java
@@ -1,10 +1,14 @@
 package com.gd.sakila.service;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.gd.sakila.mapper.CustomerMapper;
+import com.gd.sakila.vo.CustomerList;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,9 +17,20 @@ import lombok.extern.slf4j.Slf4j;
 @Transactional
 public class CustomerService {
 	@Autowired CustomerMapper customerMapper;
+	// 휴면 고객 처리 액션
 	public void modifyCustomerActiveByScheduler() {
 		log.debug("▶▶▶▶▶▶▶ modifyCustomerActiveByScheduler() 실행");
 		int row = customerMapper.updateCustomerActiveByScheduler();
 		log.debug("▶▶▶▶▶▶▶ modifyCustomerActiveByScheduler() 휴면고객 처리 행 수 : " + row);
+	}
+	
+	// 고객 목록 액션
+	public List<CustomerList> getCustomerList(Map<String, Object> map) {
+		return customerMapper.selectCustomerList(map);
+	}
+	
+	// 고객 데이터 개수 (customerTotal)
+	public int selectCustomerTotal(String searchWord) {
+		return customerMapper.selectCustomerTotal(searchWord);
 	}
 }
